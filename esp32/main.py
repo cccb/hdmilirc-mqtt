2652030d89d8ee3190838c803f7199f42c09b694
Matthias Hannig <matthias@hannig.cc>
2018-02-10 02:31:35 +0100

    implemented Camel MQTT message handling

--- esp32/main.py
+++ esp32/main.py
@@ -1,54 +1,178 @@
+
+import time
+import json
+
 import machine
 
 import config
 import wifi
 import mqtt
 
-def _mqtt_callback(topic, msg):
-    print("MQTT cb:")
-    print(topic.decode())
-    print(msg.decode())
+
+
+
+GET_CHANNEL_INPUTS_REQUEST = "GET_CHANNEL_INPUTS_REQUEST"
+GET_CHANNEL_INPUTS_SUCCESS = "GET_CHANNEL_INPUTS_SUCCESS"
+
+SET_CHANNEL_A_INPUT_REQUEST = "SET_CHANNEL_A_INPUT_REQUEST"
+SET_CHANNEL_A_INPUT_SUCCESS = "SET_CHANNEL_A_INPUT_SUCCESS"
+SET_CHANNEL_A_INPUT_ERROR = "SET_CHANNEL_A_INPUT_ERROR"
+
+SET_CHANNEL_B_INPUT_REQUEST = "SET_CHANNEL_B_INPUT_REQUEST"
+SET_CHANNEL_B_INPUT_SUCCESS = "SET_CHANNEL_B_INPUT_SUCCESS"
+SET_CHANNEL_B_INPUT_ERROR = "SET_CHANNEL_B_INPUT_ERROR"
+
+
+#
+# Action creators
+#
+def get_channel_inputs_success():
+    return {
+        "type": GET_CHANNEL_INPUTS_SUCCESS,
+        "payload": {
+            "a": hdmi.get_selected(hdmi.STATE_A_PINS),
+            "b": hdmi.get_selected(hdmi.STATE_B_PINS),
+        }
+    }
+
+
+def set_channel_a_success(next_id):
+    return {
+        "type": SET_CHANNEL_A_INPUT_SUCCESS,
+        "payload": {
+            "id": next_id,
+        },
+    }
+
+
+def set_channel_a_error(requested_id, next_id):
+    return {
+        "type": SET_CHANNEL_A_INPUT_ERROR,
+        "payload": {
+            "requested_id": requested_id,
+            "id": next_id,
+        },
+    }
+
+def set_channel_b_success(next_id):
+    return {
+        "type": SET_CHANNEL_B_INPUT_SUCCESS,
+        "payload": {
+            "id": next_id,
+        },
+    }
+
+
+def set_channel_b_error(requested_id, next_id):
+    return {
+        "type": SET_CHANNEL_B_INPUT_ERROR,
+        "payload": {
+            "requested_id": requested_id,
+            "id": next_id,
+        },
+    }
+
+
+def _make_dispatch(client):
+    def dispatch(client, action):
+        payload = json.dumps(action["payload"])
+        topic = config.mqttbasetopic + "/" + action["type"]
+
+        client.publish(topic, payload)
+
+    return dispatch
+
+
+def handle(dispatch, action):
+    payload = action["payload"]
+
+    if action["type"] == GET_CHANNEL_INPUTS_REQUEST:
+        dispatch(client, get_channel_inputs_success())
+    elif action["type"] == SET_CHANNEL_A_INPUT_REQUEST:
+        channel_id = payload.get("id", 0)
+        hdmi.select_a(channel_id)
+
+        next_id = hdmi.get_selected(hdmi.STATE_A_PINS)
+        if next_id == channel_id:
+            dispatch(set_channel_a_success(next_id))
+        else:
+            dispatch(set_channel_a_error(channel_id, next_id))
+
+    elif action["type"] == SET_CHANNEL_B_INPUT_REQUEST:
+        channel_id = payload.get("id", 0)
+        hdmi.select_a(channel_id)
+
+        next_id = hdmi.get_selected(hdmi.STATE_B_PINS)
+        if next_id == channel_id:
+            dispatch(set_channel_b_success(next_id))
+        else:
+            dispatch(set_channel_b_error(channel_id, next_id))
+
+
+def _make_mqtt_callback(client):
+    dispatch = _make_dispatch(client)
+
+    def mqtt_callback(topic, msg):
+        topic = topic.decode()
+        payload = json.loads(msg.decode())
+
+        action_type = topic.split("/")[-1]
+
+        action = {
+            "type": action_type,
+            "payload": payload,
+        }
+
+        handle(dispatch, action)
+
+    return mqtt_callback
 
 
 def _mqtt_connect(c):
-	while True:
-		try:
-			print(c.connect())
-			if not c.connect():
-				print('New session being set up')
+    while True:
+        try:
+            print(c.connect())
+            if not c.connect():
+                print('New session being set up')
                 topic = b'{}/#'.format(config.mqttbasetopic)
                 print('Subscribe to {}'.format(topic))
                 c.subscribe(topic)
-				break
-		except:
-			pass
+                break
+        except Exception as e:
+            print(e)
+            pass
+        time.sleep(0.25)
+
 
 def main():
 
     # Connect to wifi
-	w = Wifi(config.ssid, config.password, config.ifconfig)
-	if not w.connect():
+    w = wifi.Wifi(config.ssid, config.password, config.ifconfig)
+    if not w.connect():
         print("Could not connect to wifi")
-		machine.reset()
+        machine.reset()
 
-	time.sleep_ms(300)
+    time.sleep_ms(300)
 
-	c = MQTTClient('umqtt_client', config.mqttserver, port=config.mqttport)
-	c.DEBUG = True
-	c.set_callback(_mqtt_callback)
+    print("Connecting to MQTT")
+
+    c = mqtt.MQTTClient('umqtt_client',
+                        config.mqttserver,
+                        port=config.mqttport)
+    c.DEBUG = True
+    c.set_callback(_make_mqtt_callback(c))
 
     _mqtt_connect(c)
 
-	while True:
+    while True:
         c.wait_msg()
 
 
 try:
     main()
-
 except:
-	raise
-	machine.reset()
+    raise
+    machine.reset()
 
 
 
