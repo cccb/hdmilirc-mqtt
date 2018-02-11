d593526a47a7ec7b26e4b1c96b4d3a38854fd6b3
Matthias Hannig <matthias@hannig.cc>
2018-02-11 16:29:45 +0100

    added hdmi matrix simulation

--- hdmimatrix/dummy.py
+++ hdmimatrix/dummy.py
@@ -4,19 +4,35 @@
 Hdmi Matrix simulation
 """
 
+import time
+
 from llama import mqtt
+
+import hdmi
+
+DEADTIME = 1.0
+
+# Update lock timer
+LAST_UPDATE_A_FINISHED = 0
+LAST_UPDATE_B_FINISHED = 0
+
 
 # Actions
 GET_CHANNEL_INPUTS_REQUEST = "@hdmi/GET_CHANNEL_INPUTS_REQUEST"
 GET_CHANNEL_INPUTS_SUCCESS = "@hdmi/GET_CHANNEL_INPUTS_SUCCESS"
 
 SET_CHANNEL_A_INPUT_REQUEST = "@hdmi/SET_CHANNEL_A_INPUT_REQUEST"
+SET_CHANNEL_A_INPUT_START   = "@hdmi/SET_CHANNEL_A_INPUT_START"
 SET_CHANNEL_A_INPUT_SUCCESS = "@hdmi/SET_CHANNEL_A_INPUT_SUCCESS"
-SET_CHANNEL_A_INPUT_ERROR = "@hdmi/SET_CHANNEL_A_INPUT_ERROR"
+SET_CHANNEL_A_INPUT_ERROR   = "@hdmi/SET_CHANNEL_A_INPUT_ERROR"
+SET_CHANNEL_A_INPUT_CANCEL  = "@hdmi/SET_CHANNEL_A_INPUT_CANCEL"
 
 SET_CHANNEL_B_INPUT_REQUEST = "@hdmi/SET_CHANNEL_B_INPUT_REQUEST"
+SET_CHANNEL_B_INPUT_START   = "@hdmi/SET_CHANNEL_B_INPUT_START"
 SET_CHANNEL_B_INPUT_SUCCESS = "@hdmi/SET_CHANNEL_B_INPUT_SUCCESS"
-SET_CHANNEL_B_INPUT_ERROR = "@hdmi/SET_CHANNEL_B_INPUT_ERROR"
+SET_CHANNEL_B_INPUT_ERROR   = "@hdmi/SET_CHANNEL_B_INPUT_ERROR"
+SET_CHANNEL_B_INPUT_CANCEL  = "@hdmi/SET_CHANNEL_B_INPUT_CANCEL"
+
 
 #
 # Action creators
@@ -31,11 +47,29 @@ def get_channel_inputs_success():
     }
 
 
+def set_channel_a_start(next_id):
+    return {
+        "type": SET_CHANNEL_A_INPUT_START,
+        "payload": {
+            "id": next_id,
+        },
+    }
+
+
 def set_channel_a_success(next_id):
     return {
         "type": SET_CHANNEL_A_INPUT_SUCCESS,
         "payload": {
             "id": next_id,
+        },
+    }
+
+
+def set_channel_a_cancel(channel_id):
+    return {
+        "type": SET_CHANNEL_A_INPUT_CANCEL,
+        "payload": {
+            "id": channel_id,
         },
     }
 
@@ -48,6 +82,16 @@ def set_channel_a_error(requested_id, next_id):
             "id": next_id,
         },
     }
+
+
+def set_channel_b_start(next_id):
+    return {
+        "type": SET_CHANNEL_B_INPUT_START,
+        "payload": {
+            "id": next_id,
+        },
+    }
+
 
 def set_channel_b_success(next_id):
     return {
@@ -68,30 +112,73 @@ def set_channel_b_error(requested_id, next_id):
     }
 
 
+
+def set_channel_b_cancel(channel_id):
+    return {
+        "type": SET_CHANNEL_B_INPUT_CANCEL,
+        "payload": {
+            "id": channel_id,
+        },
+    }
+
+
+
+def _in_deadtime(last_update):
+    return (time.time() - last_update <= DEADTIME)
+
+
 def handle(dispatch, action):
+    global LAST_UPDATE_A_FINISHED
+    global LAST_UPDATE_B_FINISHED
+
     payload = action["payload"]
 
     if action["type"] == GET_CHANNEL_INPUTS_REQUEST:
-        dispatch(client, get_channel_inputs_success())
+        dispatch(get_channel_inputs_success())
+
     elif action["type"] == SET_CHANNEL_A_INPUT_REQUEST:
         channel_id = payload.get("id", 0)
+
+        # Check deadtime
+        if _in_deadtime(LAST_UPDATE_A_FINISHED):
+            dispatch(set_channel_a_cancel(channel_id))
+            return
+
+        # Begin update
+        dispatch(set_channel_a_start(channel_id))
+
+        # This might take a while
         hdmi.select_a(channel_id)
 
+        # Check result
         next_id = hdmi.get_selected(hdmi.STATE_A)
         if next_id == channel_id:
             dispatch(set_channel_a_success(next_id))
         else:
             dispatch(set_channel_a_error(channel_id, next_id))
 
+        LAST_UPDATE_A_FINISHED = time.time()
+
+
     elif action["type"] == SET_CHANNEL_B_INPUT_REQUEST:
         channel_id = payload.get("id", 0)
-        hdmi.select_a(channel_id)
+
+        # Check deadtime
+        if _in_deadtime(LAST_UPDATE_B_FINISHED):
+            dispatch(set_channel_b_cancel(channel_id))
+            return
+
+        # Begin
+        dispatch(set_channel_b_start(channel_id))
+        hdmi.select_b(channel_id)
 
         next_id = hdmi.get_selected(hdmi.STATE_B)
         if next_id == channel_id:
             dispatch(set_channel_b_success(next_id))
         else:
             dispatch(set_channel_b_error(channel_id, next_id))
+
+        LAST_UPDATE_B_FINISHED = time.time()
 
 
 def main(args):
@@ -103,6 +190,7 @@ def main(args):
     print("MQTT connected.")
 
     for action in receive():
+        print("Handling action: {}".format(action))
         handle(dispatch, action)
 
 
--- hdmimatrix/hdmi.py
+++ hdmimatrix/hdmi.py
@@ -23,11 +23,17 @@ def get_selected(pins):
     return -1
 
 def next_a():
-    return next_channel(STATE_A_PINS)
+    global STATE_A
+    STATE_A = next_channel(STATE_A)
+
+    return get_selected(STATE_A)
 
 
 def next_b():
-    return next_channel(STATE_B_PINS)
+    global STATE_B
+    STATE_B = next_channel(STATE_B)
+
+    return get_selected(STATE_B)
 
 
 def next_channel(state):
@@ -35,31 +41,39 @@ def next_channel(state):
     if selected == -1:
         return -1
 
-    time.sleep(0.8) # almost a second per click
+    time.sleep(0.65) # almost a second per click
 
-    state = [state[-1]]+[state[:-1]]
+    state = [state[-1]]+state[:-1]
+
     return state
 
 
-def select(state, chan):
-    if chan > 3 or chan < 0:
-        return
-
-    if chan == get_selected(state):
-        return
+def select_a(chan):
+    if chan > 3 or chan < 0 or \
+       chan == get_selected(STATE_A):
+        return -1
 
     while True:
-        next_chan = next_channel(state)
+        next_chan = next_a()
+        print("A is now: {}".format(next_chan))
         if next_chan == chan:
             return
 
         time.sleep(0.25)
 
 
-def select_a(chan):
-    select(STATE_A, chan)
-
 
 def select_b(chan):
-    select(STATE_B, chan)
+    if chan > 3 or chan < 0 or \
+       chan == get_selected(STATE_B):
+        return -1
+
+    while True:
+        next_chan = next_b()
+        print("B is now: {}".format(next_chan))
+        if next_chan == chan:
+            return
+
+        time.sleep(0.25)
+
 
