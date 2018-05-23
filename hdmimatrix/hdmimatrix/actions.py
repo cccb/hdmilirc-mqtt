03c183a0d0d1d8d708b2e219c27359202d4b1cc2
Matthias Hannig <matthias@hannig.cc>
2018-05-23 23:31:07 +0200

    refactored actions

--- hdmimatrix/hdmimatrix/actions.py
+++ hdmimatrix/hdmimatrix/actions.py
@@ -7,42 +7,35 @@ GET_SELECTED_INPUT_REQUEST = "@hdmi/GET_SELECTED_INPUT_REQUEST"
 GET_SELECTED_INPUT_SUCCESS = "@hdmi/GET_SELECTED_INPUT_SUCCESS"
 GET_SELECTED_INPUT_ERROR = "@hdmi/GET_SELECTED_INPUT_ERROR"
 
+SET_SELECTED_INPUT_REQUEST = "@hdmi/SET_SELECTED_INPUT_REQUEST"
+SET_SELECTED_INPUT_SUCCESS = "@hdmi/SET_SELECTED_INPUT_SUCCESS"
+SET_SELECTED_INPUT_ERROR = "@hdmi/SET_SELECTED_INPUT_ERROR"
+
 GET_AUDIO_MODE_REQUEST = "@hdmi/GET_AUDIO_MODE_REQUEST"
 GET_AUDIO_MODE_SUCCESS = "@hdmi/GET_AUDIO_MODE_SUCCESS"
 GET_AUDIO_MODE_ERROR = "@hdmi/GET_AUDIO_MODE_ERROR"
 
-GET_CONNECTION_STATES_REQUEST = "@hdmi/GET_CONNECTION_STATES_REQUEST"
-GET_CONNECTION_STATES_SUCCESS = "@hdmi/GET_CONNECTION_STATES_SUCCESS"
-GET_CONNECTION_STATES_ERROR = "@hdmi/GET_CONNECTION_STATES_ERROR"
+SET_AUDIO_MODE_REQUEST = "@hdmi/SET_AUDIO_MODE_REQUEST"
+SET_AUDIO_MODE_SUCCESS = "@hdmi/SET_AUDIO_MODE_SUCCESS"
+SET_AUDIO_MODE_ERROR = "@hdmi/SET_AUDIO_MODE_ERROR"
 
 GET_AUTO_SELECT_REQUEST = "@hdmi/GET_AUTO_SELECT_REQUEST"
 GET_AUTO_SELECT_SUCCESS = "@hdmi/GET_AUTO_SELECT_SUCCESS"
 GET_AUTO_SELECT_ERROR = "@hdmi/GET_AUTO_SELECT_ERROR"
 
+SET_AUTO_SELECT_REQUEST = "@hdmi/SET_AUTO_SELECT_REQUEST"
+SET_AUTO_SELECT_SUCCESS = "@hdmi/SET_AUTO_SELECT_SUCCESS"
+SET_AUTO_SELECT_ERROR = "@hdmi/SET_AUTO_SELECT_ERROR"
 
-UPDATE_SELECTED_INPUT_REQUEST = "@hdmi/UPDATE_SELECTED_INPUT_REQUEST"
-UPDATE_SELECTED_INPUT_SUCCESS = "@hdmi/UPDATE_SELECTED_INPUT_SUCCESS"
-UPDATE_SELECTED_INPUT_ERROR = "@hdmi/UPDATE_SELECTED_INPUT_ERROR"
-
-UPDATE_AUDIO_MODE_REQUEST = "@hdmi/UPDATE_AUDIO_MODE_REQUEST"
-UPDATE_AUDIO_MODE_SUCCESS = "@hdmi/UPDATE_AUDIO_MODE_SUCCESS"
-UPDATE_AUDIO_MODE_ERROR = "@hdmi/UPDATE_AUDIO_MODE_ERROR"
-
-UPDATE_AUTO_SELECT_REQUEST = "@hdmi/UPDATE_AUTO_SELECT_REQUEST"
-UPDATE_AUTO_SELECT_SUCCESS = "@hdmi/UPDATE_AUTO_SELECT_SUCCESS"
-UPDATE_AUTO_SELECT_ERROR = "@hdmi/UPDATE_AUTO_SELECT_ERROR"
-
+GET_CONNECTION_STATES_REQUEST = "@hdmi/GET_CONNECTION_STATES_REQUEST"
+GET_CONNECTION_STATES_SUCCESS = "@hdmi/GET_CONNECTION_STATES_SUCCESS"
+GET_CONNECTION_STATES_ERROR = "@hdmi/GET_CONNECTION_STATES_ERROR"
 
 # Action Creators
-def get_connection_states_success(states):
-    return {
-        "type": GET_CONNECTION_STATES_SUCCESS,
-        "payload": {
-            "connections": states,
-        }
-    }
 
-
+#
+# Selected Input
+#
 def get_selected_input_request():
     return {
         "type": GET_SELECTED_INPUT_REQUEST,
@@ -68,14 +61,87 @@ def get_selected_input_error(error):
     }
 
 
-def update_selected_input_success(input_id):
+def set_selected_input_request(input_id):
     return {
-        "type": UPDATE_SELECTED_INPUT_SUCCESS,
+        "type": SET_SELECTED_INPUT_REQUEST,
+        "payload": {
+            "input_id": input_id,
+        },
+    }
+
+
+def set_selected_input_success(input_id):
+    return {
+        "type": SET_SELECTED_INPUT_SUCCESS,
         "payload": {
             "input_id": input_id,
         }
     }
 
+
+def set_selected_input_error(input_id, error):
+    return {
+        "type": SET_SELECTED_INPUT_ERROR,
+        "payload": {
+            "input_id": input_id,
+            "error": error,
+        }
+    }
+
+#
+# Audio Mode
+#
+def get_audio_mode_request():
+    return {
+        "type": GET_AUDIO_MODE_REQUEST,
+        "payload": {},
+    }
+
+
+def get_audio_mode_success(mode_id):
+    return {
+        "type": GET_AUDIO_MODE_SUCCESS,
+        "payload": {
+            "mode_id": mode_id,
+            "mode": switch.resolve_audio_mode(mode_id),
+        },
+    }
+
+
+def get_audio_mode_error(error):
+    return {
+        "type": GET_AUDIO_MODE_ERROR,
+        "payload": {
+            "error": error,
+        },
+    }
+
+
+def set_audio_mode_request(mode_id):
+    return {
+        "type": SET_AUDIO_MODE_REQUEST,
+        "payload": {
+            "mode_id": mode_id,
+        },
+    }
+
+
+def set_audio_mode_success(mode_id):
+    return {
+        "type": SET_AUDIO_MODE_SUCCESS,
+        "payload": {
+            "mode_id": mode_id,
+        },
+    }
+
+def set_audio_mode_error(mode_id, error):
+    return {
+        "type": SET_AUDIO_MODE_ERROR,
+        "payload": {
+            "mode_id": mode_id,
+            "error": error,
+        },
+    }
 
 #
 # Auto Select
@@ -89,49 +155,63 @@ def get_auto_select_success(enabled):
     }
 
 
-def update_auto_select_success(enabled):
+def get_auto_select_error(error):
     return {
-        "type": UPDATE_AUTO_SELECT_SUCCESS,
+        "type": GET_AUTO_SELECT_ERROR,
+        "payload": {
+            "error": error,
+        },
+    }
+
+
+def set_auto_select_request(enabled):
+    return {
+        "type": SET_AUTO_SELECT_REQUEST,
         "payload": {
             "enabled": enabled,
         }
     }
 
 
-
-#
-# Audio Mode
-
-def get_audio_mode_request():
+def set_auto_select_success(enabled):
     return {
-        "type": GET_AUDIO_MODE_REQUEST,
-        "payload": {},
-    }
-
-def get_audio_mode_success(mode_id):
-    return {
-        "type": GET_AUDIO_MODE_SUCCESS,
+        "type": SET_AUTO_SELECT_SUCCESS,
         "payload": {
-            "mode_id": mode_id,
-            "mode": switch.resolve_audio_mode(mode_id),
+            "enabled": enabled,
         },
     }
 
-def get_audio_mode_error(error):
+def set_auto_select_error(error):
     return {
-        "type": GET_AUDIO_MODE_ERROR,
+        "type": SET_AUTO_SELECT_ERROR,
         "payload": {
             "error": error,
         },
     }
 
-def update_audio_mode_success(mode_id):
+#
+# Connection States
+#
+def get_connection_states_request():
     return {
-        "type": UPDATE_AUDIO_MODE_SUCCESS,
+        "type": GET_CONNECTION_STATES_REQUEST,
         "payload": {
-            "mode_id": mode_id,
-            "mode": switch.resolve_audio_mode(mode_id),
         },
     }
 
+def get_connection_states_success(states):
+    return {
+        "type": GET_CONNECTION_STATES_SUCCESS,
+        "payload": {
+            "connections": states,
+        },
+    }
+
+def get_connection_states_error(error):
+    return {
+        "type": GET_CONNECTION_STATES_ERROR,
+        "payload": {
+            "error": error,
+        },
+    }
 
